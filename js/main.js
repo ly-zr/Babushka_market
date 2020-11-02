672be9e14949eca2f3c380426a65c230007481e2
ly-zr <litvin.zakhar@gmail.com>
2020-11-02 20:45:23 +0200

    0

--- js/main.js
+++ js/main.js
@@ -87,3 +87,11 @@
         ]
       });
 }());
+
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+$page.animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+}, 800);
+return false;
+});
